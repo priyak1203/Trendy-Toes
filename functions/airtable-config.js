cdbcb9b908d0514bf71e1d04aee589638d0d3950
Priydarshini Karanth <kpriya1203@gmail.com>
2023-07-31 12:37:30 +0530

    airtable change to access token from api key

--- functions/airtable-config.js
+++ functions/airtable-config.js
@@ -1,6 +1,8 @@
 const Airtable = require('airtable-node');
 
-const airtable = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY })
+const airtable = new Airtable({
+  apiKey: process.env.AIRTABLE_PERSONAL_ACCESS_TOKEN,
+})
   .base(process.env.AIRTABLE_BASE)
   .table(process.env.AIRTABLE_TABLE);
 
